2cb7090710f6bea0d19501fe29befecc333d29c9
André Scherrer <andre@computerscience.com.br>
2019-11-18 15:55:39 -0300

    Cadastro -> Validação

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'da8b160e67ed39e61a193170fd158655',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'gympoint',
+  database: 'gympoint',
+  port: '5454',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
